237bbf9fbd028c08902fc9a8bf10aa466871d886
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-10-15 15:56:04 -0300

    imagem e cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:agenda_de_contatos/ui/home_page.dart';
 import 'package:flutter/material.dart';
-import './ui//home_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage,
+    title: 'agenda de contatos',
+    home: HomePage(),
   ));
 }
